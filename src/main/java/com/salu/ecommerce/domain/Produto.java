1334ba8eeb69472c3963fef276cfb49c3159e9fc
LucasSalu <samuel_6000@outlook.com>
2021-06-26 02:58:18 -0300

    atualização para definir jsonIgnore como padrao

--- src/main/java/com/salu/ecommerce/domain/Produto.java
+++ src/main/java/com/salu/ecommerce/domain/Produto.java
@@ -29,7 +29,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",  // O NOME DA TABELA
 				joinColumns = @JoinColumn(name = "produto_id"),   //Nome do da chave
